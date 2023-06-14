f6f9a11947ad6565079f15a60e7250c47a97be04
Sergio-Prada-369 <alejanprada140705@gmail.com>
2023-06-14 12:01:15 -0500

    feat: :sparkles: finish

---
+++ E-commerce/views/modules/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.2
+    </div>
+    <strong>Copyright &copy; 20203 <a href="https://github.com/Sergio-Prada-963">Sergio Prada</a>.</strong> All rights reserved.
+</footer>
